1110c60d18601e9051cbae1899446126702dbb68
Sarvesh Malandkar <91262743+Codeenk@users.noreply.github.com>
2025-02-22 16:54:16 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -34,7 +34,7 @@ function Home() {
           transition={{ duration: 0.8 }}
           className="container mx-auto px-4 text-center"
         >
-          <h1 className="text-4xl md:text-8xl font-bold mb-6 bg-clip-text text-transparent bg-gradient-to-r from-blue-400 to-purple-600 typing-animation">
+          <h1 className="text-5xl md:text-8xl font-bold mb-6 bg-clip-text text-transparent bg-gradient-to-r from-blue-400 to-purple-600 typing-animation">
             Sarvesh Malandkar
           </h1>
           <p className="text-xl md:text-2xl text-gray-300 mb-8">
